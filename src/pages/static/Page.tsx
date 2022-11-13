43bce71b589102ecf692f6c813bf3f18685d8390
Remonone <50263589+Remonone@users.noreply.github.com>
2022-11-13 23:30:36 +0200

    Small set up project

--- src/pages/static/Page.tsx
+++ src/pages/static/Page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Footer from '../../components/Footer/Footer'
+import Header from '../../components/Header/Header'
 
 const Page = (props: {element: React.ReactNode}) => {
   return (
     <div>
-        <div>Header</div>
+        <Header/>
         {props.element}
-        <div>Footer</div>
+        <Footer/>
     </div>
   )
 }
